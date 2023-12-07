791b4a4c88601b684bf020d697de78dbea4eb473
basu sahu <basusahu1201@gmail.com>
2023-12-07 16:41:30 +0530

    String problem

---
+++ src/deddf.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class deddf {
+public static void main(String[] args) {
+Scanner scan = new Scanner(System.in);
+int n = scan.nextInt();
+for(int i =0; i<=n; i++)
+{
+ if(i==4)
+ {
+	 i=25;
+	
+ }
+ System.out.println(i);
+}
+}
+}
+//if(n=)
+//}
+//}
