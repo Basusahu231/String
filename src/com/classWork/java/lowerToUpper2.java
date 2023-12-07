71ecdfff9bb2794271e8fdd55ac20167e731fb24
basu sahu <basusahu1201@gmail.com>
2023-12-07 16:40:39 +0530

    String problem

--- src/com/classWork/java/lowerToUpper2.java
+++ src/com/classWork/java/lowerToUpper2.java
@@ -1,30 +1,31 @@
 package com.classWork.java;
 import java.util.*;
 public class lowerToUpper2 {
-public static void main(String[] args) {
-	Scanner scan = new Scanner(System.in);
-	System.out.println("Enter the string ");
-	String name = scan.nextLine();
-	char [] ch = name.toCharArray();
-	for(int i =0; i<=ch.length-1; i++)
-	{
-		if(i==0)
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the string ");
+		String name = scan.nextLine();
+		char [] ch = name.toCharArray();
+		for(int i =0; i<=ch.length-1; i++)
 		{
-			if(ch[i]>='a' && ch[i]<='z' )
+			if(i==0)
 			{
-				ch[i] = (char)(ch[i]-32);
+				if(ch[i]>='a' && ch[i]<='z' )
+				{
+					ch[i] = (char)(ch[i]-32);
+				}
 			}
-		}
-		else if(ch[i]==' ')
-		{
-			if(ch[i]>='a' && ch[i]<='z')
+			else if(ch[i]==' ')
 			{
-				ch[i] = (char)(ch[i]-32);
+				i++;
+				if(ch[i]>='a' && ch[i]<='z')
+				{
+					ch[i] = (char)(ch[i]-32);
+				}
 			}
+
 		}
-	
+		String newstr = new String(ch);
+		System.err.println(newstr);
 	}
-	String newstr = new String(ch);
-	System.err.println(newstr);
-}
 }
