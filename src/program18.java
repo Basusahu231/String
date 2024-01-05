f3d7887e3f7990e21b5b97da9a3303815bf538ac
basu sahu <basusahu1201@gmail.com>
2024-01-05 22:39:30 +0530

    String problem

--- src/program18.java
+++ src/program18.java
@@ -1,4 +1,17 @@
 
 public class program18 {
-
+	public static void main(String[] args) {
+	String s = "please wait,continue to fight,continue to win";
+	String[] arr =s.split(" ");
+	int count = 0;
+	for(int i =0; i<=arr.length-1; i++)
+	{
+		if(arr[i]==",")
+		{
+			i++;
+		}
+		count++;
+	}
+	System.out.println(count );
+}
 }
