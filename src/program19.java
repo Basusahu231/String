405a1a0d934172322883bc7dd2212c18bb87b23a
basu sahu <basusahu1201@gmail.com>
2024-01-07 23:24:05 +0530

    String problem

---
+++ src/program19.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class program19 {
+public static void main(String[] args) {
+Scanner scan = new Scanner(System.in);
+System.out.println("enter the number");
+int n = scan.nextInt();
+String s = null;
+for(int i =0; i<=n;i++)
+{
+	 s = scan.next();
+}
+for(int i = 0; i<=s.length()-1; i++)
+{
+	if(i%2==0)
+	{
+		char even = s.charAt(i);
+	}
+}
+}
+}
